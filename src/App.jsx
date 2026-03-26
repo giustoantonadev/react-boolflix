7e7f4165d4464e9f3e1c866f42890215a79960d5
giustoantonadev <giustoantonadev@gmail.com>
2026-03-26 10:39:11 +0100

    Add .env and import API

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@
 
 function App() {
 
+  const API_KEY = import.meta.env.VITE_API_KEY
+
+  console.log(API_KEY);
+
+  const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=ritorno+al+fut`
+
+  console.log(api_url);
+
 
   return (
     <>
- 
- 
- 
+
+
+
 
     </>
   )
