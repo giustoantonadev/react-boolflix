83a353210abb4c91a8435b2421a26b6ff989b6ee
giustoantonadev <giustoantonadev@gmail.com>
2026-03-26 10:54:22 +0100

    create state to store api results get api results with useeffect to not get a loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 
 
 
@@ -5,11 +6,22 @@ function App() {
 
   const API_KEY = import.meta.env.VITE_API_KEY
 
-  console.log(API_KEY);
-
   const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=ritorno+al+fut`
 
-  console.log(api_url);
+  const [film, setFilm] = useState([])
+
+
+  useEffect(() => {
+    fetch(api_url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.results);
+        setFilm(data.results)
+      })
+
+  }, [])
+
+
 
 
   return (
