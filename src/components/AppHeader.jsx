fa017ba13358fbd4ffea08e74cdca2a2921e3731
giustoantonadev <giustoantonadev@gmail.com>
2026-04-27 16:48:34 +0200

    add buttons for Movies and Tv Series in AppHeader component

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -9,6 +9,8 @@ export default function AppHeader({ search, setSearch, handleSearch, handleHome 
                     <div className="navbar-brand text-danger d-flex align-items-center" onClick={handleHome} style={{ cursor: "pointer" }}                    >
                         <h4 className="m-0">BOOLFLIX</h4>
                     </div>
+                    <button className="btn btn-danger">Movies</button>
+                    <button className="btn btn-danger">Tv Series</button>
                     <form onSubmit={handleSearch} className="d-flex" role="search">
                         <input
                             value={search} onChange={(e) => setSearch(e.target.value)}
